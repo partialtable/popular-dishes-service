4ac33993133930d6922e7fc87ef0c15ad7026214
Jinyeong Park <jinyeongpark1205@gmail.com>
2020-10-15 13:18:52 -0700

    Add Postgres schema

---
+++ db/postgres/schema.sql
@@ -0,0 +1,36 @@
+-- postgreSQL database schema
+
+DROP DATABASE IF EXISTS dishes_service;
+
+CREATE DATABASE dishes_service;
+
+CREATE TABLE restaurants (
+  restaurant_id SERIAL PRIMARY KEY NOT NULL,
+  name CHAR(50)
+);
+
+CREATE TABLE dishes (
+  dish_id SERIAL PRIMARY KEY NOT NULL,
+  restaurant_id INT NOT NULL REFERENCES restaurants(restaurant_id),
+  name TEXT,
+  ingredients TEXT,
+  picture TEXT
+);
+
+CREATE TABLE users (
+  user_id SERIAL PRIMARY KEY NOT NULL,
+  name TEXT,
+  avatar_url TEXT,
+  first_name TEXT,
+  last_name TEXT
+);
+
+CREATE TABLE reviews (
+  review_id SERIAL PRIMARY KEY NOT NULL,
+  dish_id INT NOT NULL REFERENCES dishes(dish_id),
+  user_id INT NOT NULL REFERENCES users(user_id),
+  review TEXT,
+  dined_on DATE,
+  stars INT,
+  user_status BOOLEAN
+);
